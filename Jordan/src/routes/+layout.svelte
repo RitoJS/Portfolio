9bfd191d7b2f1bf54d710c16a088054b8fc0393e
RitoJS <jsscottsama@gmail.com>
2026-07-07 12:04:51 +0200

    Transformation du canvas en composant + ajout  du composant dans le layout

--- Jordan/src/routes/+layout.svelte
+++ Jordan/src/routes/+layout.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import CanvasBackground from '$lib/components/canvas-background.svelte';
 
 	let { children } = $props();
 </script>
-
+<CanvasBackground></CanvasBackground>
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 {@render children()}
