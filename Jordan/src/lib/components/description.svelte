74c11ffb85d7cdee39a2c62ec9fe443beb674973
RitoJS <jsscottsama@gmail.com>
2026-07-08 11:34:51 +0200

    description component

--- Jordan/src/lib/components/description.svelte
+++ Jordan/src/lib/components/description.svelte
@@ -11,14 +11,30 @@
     });
 </script>
 
-{#if hydrated && info.text}
-    <AnimationLetter 
-        data={info.text} 
-        className="title-text"
-    />
-{:else}
-    <AnimationLetter 
-        data="Une description sans hover !" 
-        className="title-text"
-    />
-{/if}
+<div class="desc-block hidden md:flex ">
+    <div class="block-decorator flex">
+        <div class="thick-line"></div>
+        <div class="thin-line"></div>
+    </div>
+    <div>
+         {#if hydrated && info.text}
+            <AnimationLetter 
+                data={info.text} 
+                className="title-text"
+            />
+        {:else}
+            <AnimationLetter 
+                data="Une description sans hover !" 
+                className="title-text"
+            />
+        {/if}
+    </div>
+   
+</div>
+
+<style>
+    .desc-block {
+        background-color: var(--default-bg-elements2);
+        box-shadow: 3px 3px 0px var(--shadow-elements-color);
+    }
+</style>
